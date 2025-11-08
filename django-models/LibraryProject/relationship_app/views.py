c5a98edc6ca26ddf0886881a81e6ef6ff6b94eb5
shady-cj <petersp2000@gmail.com>
2025-11-08 14:13:12 +0100

    template restructure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Book, Library
+
+# from .models import Library
 # Create your views here.
 
 
