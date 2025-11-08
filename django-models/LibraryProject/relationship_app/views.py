7ae9fa76e54aae032fa16b024392164ab9ce6f7e
shady-cj <petersp2000@gmail.com>
2025-11-08 14:10:23 +0100

    template restructure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryListView(ListView):
     model = Library
