16a3b04a1d1bdf2804c6578fe46c9c9c4bcce514
shady-cj <petersp2000@gmail.com>
2025-11-08 11:38:31 +0100

    tweak query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,8 +5,8 @@ from .models import Book, Author, Librarian, Library
 
 all_books_by_author_1 = Book.objects.filter(author__id=1)
 
-
-library_1 = Library.objects.get(id=1)
+library_name = "new library"
+library_1 = Library.objects.get(name=library_name)
 
 all_library_1_books = library_1.books.all()
 
