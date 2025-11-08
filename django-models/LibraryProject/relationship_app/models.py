5fdb545b49aa267d0507e81a1cfc2b0bead50b98
shady-cj <petersp2000@gmail.com>
2025-11-08 11:36:44 +0100

    added string represenation method to models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,13 +5,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=64)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Book {self.title} by {self.author.name}"
+
+
 class Library(models.Model):
     name = models.CharField(max_length=64)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Librarian(models.Model):
