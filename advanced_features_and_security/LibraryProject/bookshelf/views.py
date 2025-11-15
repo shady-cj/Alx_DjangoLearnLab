d761b6e113029325c86490d5c19d1d6d958d062b
shady-cj <petersp2000@gmail.com>
2025-11-15 20:05:21 +0100

    adding forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, HttpResponse
 from django.contrib.auth.decorators import permission_required
-
+from .forms import BookForm
 # Create your views here.
 from .models import Book
 
@@ -19,20 +19,17 @@ def view_book(request, pk=None):
 def list_books(request):
     if request.method == "GET":
         books = get_list_or_404(Book.objects.all())
-        result = ""
-        for book in books:
-            result += f"{book.title} by {book.author}\n"
-        return HttpResponse(f"List of boks\n {result}")
+        return render(request, "bookshelf/book_list.html", {"books": books})
     
 @permission_required("bookshelf.can_create", raise_exception=True)
 def create_book(request):
+    form = BookForm()
     if request.method == "POST":
-        book_title = request.POST.get('title')
-        book_author = request.POST.get('author')
-        book_year = request.POST.get('publication_year')
-        Book.objects.create(title=book_title, author=book_author, publication_year=book_year)
-
-        return HttpResponse(f"Book created successfully", 201)
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(f"Book created successfully", 201)
+    return render(request, "bookshelf/form_example.html", {"form": form})
 
 @permission_required("bookshelf.can_delete", raise_exception=True)
 def delete_book(request, pk=None):
