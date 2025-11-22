eab091bbb80aa016b87b22c4252a448bd8abac2b
shady-cj <petersp2000@gmail.com>
2025-11-22 19:52:57 +0100

    added viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
+# viewsets.ModelViewSet
 # generics.ListAPIView
 
 from .serializers import BookSerializer
