b4306ae9a937da6a021fc4af7f330573190056ec
shady-cj <petersp2000@gmail.com>
2025-11-22 19:38:57 +0100

    added serializers, views and routers

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Book
 
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
