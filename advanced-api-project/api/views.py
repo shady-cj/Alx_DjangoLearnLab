4921c64b63a20ca6f5efc236bd7b59b99219fc24
shady-cj <petersp2000@gmail.com>
2025-11-30 21:06:33 +0100

    using django filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from rest_framework import filters
 from .models import Book, Author
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 from .serializers import BookSerializer, AuthorSerializer
@@ -16,6 +17,10 @@ class BookListView(ListAPIView):
         List all books in the database
     """
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['publication_year', 'title']
+    filterset_fields = ['publication_year', 'author__name']
     # This will ensure that only authenticated users can access this view
     queryset = Book.objects.all()
     serializer_class = BookSerializer
